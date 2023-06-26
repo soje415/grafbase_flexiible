96d279b3b0d259c037bc32d075462809b487fa91
Sojeee <soje415@gmail.com>
2023-06-26 12:28:55 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return  (
+        <section className="flex-start flex-col paddings mb-16">
+            <h1>Catergories</h1>
+            <h1>posts</h1>
+            <h1>LoadMore</h1>
+        </section>
+    )
+}
+
+export default Home;
